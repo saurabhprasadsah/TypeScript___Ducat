e8ed5e3a8c30e3f3570d3d0a7fc5dee427e5a5ea
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-08-14 23:26:32 +0530

    types.ts

--- Types/types.js
+++ Types/types.js
@@ -1,1 +1,15 @@
-console.log("hello types");
+// console.log("hello types");
+//--Types
+//-string
+var string = "saurabh";
+console.log(string);
+//--number
+var number = 56;
+console.log(number);
+//boolean
+var boolean = true;
+console.log(boolean);
+var num = [56, 65, 98];
+var str = ["saurabh", "Raju", "kaju"];
+console.log(typeof str, str);
+console.log(typeof num, num);
--- Types/types.ts
+++ Types/types.ts
@@ -1,1 +1,23 @@
-console.log("hello types");
+// console.log("hello types");
+
+
+// //--Types
+// //-string
+// let string: string ="saurabh";
+// console.log(string);
+
+// //--number
+// let number:Number =56;
+// console.log(number);
+
+// //boolean
+// let boolean:boolean =true;
+// console.log(boolean);
+
+// let num: number[] = [56,65,98]
+// let str:string[] =["saurabh","Raju","kaju"]
+
+// console.log(typeof str,str);
+// console.log(typeof num,num);
+
+
